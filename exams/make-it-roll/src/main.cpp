fad0a325441227436e63482588cd94237017b341
Ugo Pattacini <ugo.pattacini@iit.it>
2014-07-07 12:13:39 +0200

    exams added

---
+++ exams/make-it-roll/src/main.cpp
@@ -0,0 +1,257 @@
+#include <string>
+#include <stdio.h>
+
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/math/Math.h>
+
+#include <iCub/ctrl/math.h>
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::dev;
+using namespace yarp::sig;
+using namespace yarp::math;
+using namespace iCub::ctrl;
+
+
+/***************************************************/
+class CtrlModule: public RFModule
+{
+protected:
+    PolyDriver drvArm, drvGaze;
+    ICartesianControl *iarm;
+    IGazeControl      *igaze;
+
+    BufferedPort<ImageOf<PixelRgb>> imgLPortIn,imgRPortIn;
+    Port imgLPortOut,imgRPortOut;
+    RpcServer rpcPort;
+    
+    Mutex mutex;
+    Vector cogL,cogR;
+    bool okL,okR;
+
+    /***************************************************/
+    bool getCOG(ImageOf<PixelRgb> &img, Vector &cog)
+    {
+        int xMean=0;
+        int yMean=0;
+        int ct=0;
+
+        for (int x=0; x<img.width(); x++)
+        {
+            for (int y=0; y<img.height(); y++)
+            {
+                PixelRgb &pixel=img.pixel(x,y);
+                if ((pixel.b>5.0*pixel.r) && (pixel.b>5.0*pixel.g))
+                {
+                    xMean+=x;
+                    yMean+=y;
+                    ct++;
+                }
+            }
+        }
+
+        if (ct>0)
+        {
+            cog.resize(2);
+            cog[0]=xMean/ct;
+            cog[1]=yMean/ct;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    /***************************************************/
+    Vector retrieveTarget3D(const Vector &cogL, const Vector &cogR)
+    {
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void fixate(const Vector &x)
+    {
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    Vector computeHandOrientation()
+    {
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void approachTargetWithHand(const Vector &x, const Vector &o)
+    {
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void makeItRoll(const Vector &x, const Vector &o)
+    {
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void look_down()
+    {
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void roll(const Vector &cogL, const Vector &cogR)
+    {
+        printf("detected cogs = (%s) (%s)\n",
+                cogL.toString(0,0).c_str(),cogR.toString(0,0).c_str());
+
+        Vector x=retrieveTarget3D(cogL,cogR);
+        printf("retrieved 3D point = (%s)\n",x.toString(3,3).c_str());
+
+        fixate(x);
+        printf("fixating at (%s)\n",x.toString(3,3).c_str());
+
+        Vector o=computeHandOrientation();
+        printf("computed orientation = (%s)\n",o.toString(3,3).c_str());
+
+        approachTargetWithHand(x,o);
+        printf("approached\n");
+
+        makeItRoll(x,o);
+        printf("roll!\n");
+    }
+
+    /***************************************************/
+    void home()
+    {
+        // FILL IN THE CODE
+    }
+
+public:
+    /***************************************************/
+    bool configure(ResourceFinder &rf)
+    {
+        // FILL IN THE CODE
+
+        imgLPortIn.open("/imgL:i");
+        imgRPortIn.open("/imgR:i");
+
+        imgLPortOut.open("/imgL:o");
+        imgRPortOut.open("/imgR:o");
+
+        rpcPort.open("/service");
+        attach(rpcPort);
+
+        return true;
+    }
+
+    /***************************************************/
+    bool interruptModule()
+    {
+        imgLPortIn.interrupt();
+        imgRPortIn.interrupt();
+        return true;
+    }
+
+    /***************************************************/
+    bool close()
+    {
+        drvArm.close();
+        drvGaze.close();
+        imgLPortIn.close();
+        imgRPortIn.close();
+        imgLPortOut.close();
+        imgRPortOut.close();
+        rpcPort.close();
+        return true;
+    }
+
+    /***************************************************/
+    bool respond(const Bottle &command, Bottle &reply)
+    {
+        string cmd=command.get(0).asString().c_str();
+        if (cmd=="look_down")
+        {
+            look_down();
+            reply.addString("Yep! I'm looking down now!");
+            return true;
+        }
+        else if (cmd=="roll")
+        {
+            // FILL IN THE CODE
+
+            if (...)
+            {
+                roll(cogL,cogR);
+                reply.addString("Yeah! I've made it roll like a charm!");
+            }
+            else
+                reply.addString("I don't see any object!");
+
+            return true;
+        }
+        else if (cmd=="home")
+        {
+            home();
+            reply.addString("I've got the hard work done! Going home.");
+            return true;
+        }
+        else
+            return RFModule::respond(command,reply);
+    }
+
+    /***************************************************/
+    double getPeriod()
+    {
+        return 0.0;     // sync upon incoming images
+    }
+
+    /***************************************************/
+    bool updateModule()
+    {
+        // get fresh images
+        ImageOf<PixelRgb> *imgL=imgLPortIn.read();
+        ImageOf<PixelRgb> *imgR=imgRPortIn.read();
+
+        // interrupt sequence detected
+        if ((imgL==NULL) || (imgR==NULL))
+            return false;
+
+        // compute the center-of-mass of pixels of our color
+        mutex.lock();
+        okL=getCOG(*imgL,cogL);
+        okR=getCOG(*imgR,cogR);
+        mutex.unlock();
+
+        PixelRgb color;
+        color.r=255; color.g=0; color.b=0;
+
+        if (okL)
+            draw::addCircle(*imgL,color,(int)cogL[0],(int)cogL[1],5);
+
+        if (okR)
+            draw::addCircle(*imgR,color,(int)cogR[0],(int)cogR[1],5);
+
+        imgLPortOut.write(*imgL);
+        imgRPortOut.write(*imgR);
+
+        return true; 
+    }
+};
+
+
+/***************************************************/
+int main()
+{   
+    Network yarp;
+    if (!yarp.checkNetwork())
+        return -1;
+
+    CtrlModule mod;
+    ResourceFinder rf;
+    return mod.runModule(rf);
+}
+
+
+
