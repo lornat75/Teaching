d7d2251d7ab76cdad92ac22904cebf99add0d1ba
Lorenzo <lornat75@gmail.com>
2015-06-08 06:59:45 -0700

    rename

---
+++ code/icub-vision/build/CMakeFiles/move_ball.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/move_ball.dir/move_ball.cpp.o"
+  "move_ball.pdb"
+  "move_ball"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/move_ball.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ code/icub-vision/build/CMakeFiles/object_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/object_detector.dir/object_detector.cpp.o"
+  "object_detector.pdb"
+  "object_detector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/object_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
