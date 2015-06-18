f17f9682e48f6666ebc2196064d1c9727087a737
Lorenzo Natale <lorenzo.natale@iit.it>
2015-06-18 14:48:24 +0200

    turtlesim

---
+++ code-templates/ros-turtlesim/Readme.txt
@@ -0,0 +1,20 @@
+First parse .msg.
+
+In this order:
+
+yarpidl_rosmsg Vector3.msg
+yarpidl_rosmsg Twist.msg
+yarpidl_rosmsg Pose.msg
+
+Compile:
+
+mkdir build
+cd build 
+cmake ../
+
+First run ./turtle (this program)
+rosrun turtlesim turtlesim_node
+
+
+
+
---
+++ code-templates/ros-turtlesim/main.cpp
@@ -0,0 +1,22 @@
+#include "Pose.h"
+#include "Twist.h"
+#include <iostream>
+#include <yarp/os/Network.h>
+#include <yarp/os/Time.h>
+
+#include <yarp/os/Node.h>
+#include <yarp/os/Subscriber.h>
+#include <yarp/os/Publisher.h>
+
+using namespace std;
+int main()
+{
+   yarp::os::Network network;
+
+   cout<<"Starting sender\n";
+   while(true)
+   {
+      
+   }
+   return 0;
+}
